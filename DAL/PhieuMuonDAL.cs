bf1c24346f15cab02bc3c5f24dc3e2ec6274f06d
VO THANH LAM <vothanhlam@Mac-cua-Lam.local>
2026-01-18 13:52:13 +0700

    sua phieumuondal

--- DAL/PhieuMuonDAL.cs
+++ DAL/PhieuMuonDAL.cs
@@ -6,24 +6,53 @@ namespace QuanLyThuVienSo.API.DAL
     public class PhieuMuonDAL
     {
         private readonly QuanLyThuVienSoContext _context;
-        public PhieuMuonDAL(QuanLyThuVienSoContext context) { _context = context; }
 
-        // Mượn DB Context để BUS quản lý Transaction (Cách đơn giản nhất cho đồ án)
-        public QuanLyThuVienSoContext Context => _context; 
+        public PhieuMuonDAL(QuanLyThuVienSoContext context)
+        {
+            _context = context;
+        }
+
+        public QuanLyThuVienSoContext Context => _context;
+
+        public async Task<List<PhieuMuon>> GetAll()
+        {
+            return await _context.PhieuMuons
+                .Include(x => x.DocGia) // Đã sửa: MaDocGiaNavigation -> DocGia
+                .Include(x => x.ChiTietPhieuMuons)
+                .ThenInclude(x => x.Sach) // Đã sửa: MaSachNavigation -> Sach
+                .OrderByDescending(x => x.NgayMuon)
+                .ToListAsync();
+        }
 
         public async Task<PhieuMuon?> GetById(int id)
         {
             return await _context.PhieuMuons
-                .Include(p => p.ChiTietPhieuMuons)
-                .FirstOrDefaultAsync(p => p.MaPhieu == id);
+                .Include(x => x.DocGia) // Đã sửa
+                .Include(x => x.ChiTietPhieuMuons)
+                .ThenInclude(x => x.Sach) // Đã sửa
+                .FirstOrDefaultAsync(x => x.MaPhieu == id);
         }
 
-        public async Task<List<PhieuMuon>> GetHistory(string maDocGia)
+        public async Task Add(PhieuMuon pm)
         {
-            return await _context.PhieuMuons
-                .Where(p => p.MaDocGia == maDocGia)
-                .Include(p => p.ChiTietPhieuMuons).ThenInclude(ct => ct.MaSachNavigation)
-                .OrderByDescending(p => p.NgayMuon).ToListAsync();
+            _context.PhieuMuons.Add(pm);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(PhieuMuon pm)
+        {
+            _context.PhieuMuons.Update(pm);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var pm = await GetById(id);
+            if (pm != null)
+            {
+                _context.PhieuMuons.Remove(pm);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
